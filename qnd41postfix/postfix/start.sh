9572d7fa6330427b1a163be2ee7a364a4871a296
smartquailstage <smartquail.stage@gmail.com>
2024-08-08 12:50:26 -0500

    Update start.sh

--- qnd41postfix/postfix/start.sh
+++ qnd41postfix/postfix/start.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+# Configuración de las credenciales de PostgreSQL
+export PGPASSWORD="smartquaildev1719pass"
+export PGUSER="sqadmindb"
+export POSTFIX_POSTGRES_DB="POSFIXDB"
+export POSTFIX_POSTGRES_USER="sqadmindb"
+export POSTFIX_POSTGRES_HOST="smartquaildb"
+
 function log {
   echo "$(date) $ME - $@"
 }
 
 function addUserInfo {
-  # Add user 'info' if it doesn't exist
   if ! id -u info &>/dev/null; then
     log "Adding user 'info'"
     adduser -D -H info
@@ -14,56 +20,38 @@ function addUserInfo {
   fi
 }
 
-export POSTFIX_POSTGRES_DB=${POSTFIX_POSTGRES_DB}
-export POSTFIX_POSTGRES_PASSWORD=${POSTFIX_POSTGRES_PASSWORD}
-export POSTFIX_POSTGRES_USER=${POSTFIX_POSTGRES_USER}
-export POSTFIX_POSTGRES_HOST=${POSTFIX_POSTGRES_HOST}
+function createTable {
+  local table_name=$1
+  local table_sql=$2
 
-
-function createVirtualDomainsTable {
-  log "Creating virtual_domains table in PostgreSQL..."
-  psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "CREATE TABLE IF NOT EXISTS virtual_domains (
-    id SERIAL PRIMARY KEY,
-    domain VARCHAR(255) NOT NULL UNIQUE
-  );"
-
-  log "Creating virtual_mailbox_domains table in PostgreSQL..."
-  psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "CREATE TABLE IF NOT EXISTS virtual_mailbox_domains (
-    id SERIAL PRIMARY KEY,
-    domain VARCHAR(255) NOT NULL UNIQUE
-  );"
+  log "Creating ${table_name} table in PostgreSQL..."
+  PGPASSWORD=$PGPASSWORD psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "$table_sql"
+  
+  if [ $? -eq 0 ]; then
+    log "${table_name} table created successfully."
+  else
+    log "Failed to create ${table_name} table."
+  fi
 }
 
-function createVirtualAliasesTable {
-  log "Creating virtual_aliases table in PostgreSQL..."
-  psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "CREATE TABLE IF NOT EXISTS virtual_aliases (
-    id SERIAL PRIMARY KEY,
-    source VARCHAR(255) NOT NULL,
-    destination VARCHAR(255) NOT NULL
-  );"
-}
-
-function createVirtualMailboxesTable {
-  log "Creating virtual_mailboxes table in PostgreSQL..."
-  psql -U "$POSTFIX_POSTGRES_USER" -d "$POSTFIX_POSTGRES_DB" -h "$POSTFIX_POSTGRES_HOST" -c "CREATE TABLE IF NOT EXISTS virtual_mailboxes (
-    id SERIAL PRIMARY KEY,
-    mailbox VARCHAR(255) NOT NULL UNIQUE
-  );"
+function createVirtualTables {
+  createTable "virtual_domains" "CREATE TABLE IF NOT EXISTS virtual_domains (id SERIAL PRIMARY KEY, domain VARCHAR(255) NOT NULL UNIQUE);"
+  createTable "virtual_mailbox_domains" "CREATE TABLE IF NOT EXISTS virtual_mailbox_domains (id SERIAL PRIMARY KEY, domain VARCHAR(255) NOT NULL UNIQUE);"
+  createTable "virtual_aliases" "CREATE TABLE IF NOT EXISTS virtual_aliases (id SERIAL PRIMARY KEY, source VARCHAR(255) NOT NULL, destination VARCHAR(255) NOT NULL);"
+  createTable "virtual_mailboxes" "CREATE TABLE IF NOT EXISTS virtual_mailboxes (id SERIAL PRIMARY KEY, mailbox VARCHAR(255) NOT NULL UNIQUE);"
 }
 
 function serviceConf {
-  # Check hostname variable
-  if [[ ! ${HOSTNAME} =~ \. ]]; then
-    HOSTNAME=$HOSTNAME.$DOMAIN
+  if [[ ! $HOSTNAME =~ \. ]]; then
+    HOSTNAME="$HOSTNAME.$DOMAIN"
   fi
 
-  # Substitute configuration
   for VARIABLE in $(env | cut -f1 -d=); do
     VAR=${VARIABLE//:/_}
-    sed -i "s={{ $VAR }}=${!VAR}=g" /etc/postfix/*.cf
+    VALUE=${!VAR}
+    sed -i "s|{{ $VAR }}|$VALUE|g" /etc/postfix/*.cf
   done
 
-  # Override Postfix configuration
   if [ -f /overrides/postfix.cf ]; then
     while read -r line; do
       [[ -n "$line" && "$line" != [[:blank:]#]* ]] && postconf -e "$line"
@@ -73,7 +61,6 @@ function serviceConf {
     echo "No extra postfix settings loaded because optional '/overrides/postfix.cf' not provided."
   fi
 
-  # Include table-map files
   if ls -A /overrides/*.map 1> /dev/null 2>&1; then
     cp /overrides/*.map /etc/postfix/
     postmap /etc/postfix/*.map
@@ -88,11 +75,8 @@ function serviceConf {
 
 function serviceStart {
   addUserInfo
-  createVirtualDomainsTable    # Crear tablas necesarias
-  createVirtualAliasesTable   # Crear tabla virtual_aliases
-  createVirtualMailboxesTable # Crear tabla virtual_mailboxes
+  createVirtualTables
   serviceConf
-  # Iniciar Postfix
   log "[ Iniciando Postfix... ]"
   /usr/sbin/postfix start-fg
 }
