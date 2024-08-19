fe8eb859615d6384eeb8097eda7ef313033600d9
smartquailstage <smartquail.stage@gmail.com>
2024-08-19 14:45:47 -0500

    Update start.sh

--- qnd41postfix/postfix/start.sh
+++ qnd41postfix/postfix/start.sh
@@ -60,7 +60,7 @@ function insertInitialData {
   local insert_sql="
     INSERT INTO virtual_domains (domain) VALUES ('mail.smartquail.io') ON CONFLICT DO NOTHING;
     INSERT INTO virtual_users (email, password) VALUES ('info@mail.smartquail.io', 'ms95355672') ON CONFLICT DO NOTHING;
-    INSERT INTO virtual_aliases (source, destination) VALUES ('info@mail.smartquail.io', 'info@mail.smartquail.io') ON CONFLICT DO NOTHING;
+    INSERT INTO virtual_aliases (source, destination) VALUES ('info@mail.smartquail.io', 'info');
     INSERT INTO virtual_mailboxes (username ,email, maildir) VALUES ('info','info@mail.smartquail.io', '/var/mail/info@mail.smartquail.io') ON CONFLICT DO NOTHING;
     INSERT INTO virtual_mailbox_domains (domain) VALUES ('mail.smartquail.io') ON CONFLICT DO NOTHING;
   "
