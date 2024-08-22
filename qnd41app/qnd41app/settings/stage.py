e1852319094b3c9299f7e2a369a5c230780d5b81
smartquailstage <smartquail.stage@gmail.com>
2024-08-22 11:05:24 -0500

    Update stage.py

--- qnd41app/qnd41app/settings/stage.py
+++ qnd41app/qnd41app/settings/stage.py
@@ -249,6 +249,6 @@ EMAIL_PORT          =  os.environ.get('EMAIL_PORT')
 EMAIL_HOST_USER     = os.environ.get('EMAIL_HOST_USER ')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL  = EMAIL_HOST_USER
-EMAIL_BACKEND       = os.environ.get('EMAIL_BACKEND')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS       = True
 EMAIL_USE_SSL       = False
