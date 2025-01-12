692c1efac2d852cba7e9239efc8528e94ed74a93
smartquailstage <smartquail.stage@gmail.com>
2025-01-12 13:34:34 -0500

    Update base.py

--- qnd41app/qnd41app/settings/base.py
+++ qnd41app/qnd41app/settings/base.py
@@ -162,6 +162,18 @@ MIDDLEWARE = [
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
 
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
+STATICFILES_DIRS = [BASE_DIR / "staticfiles"]  
+STATIC_URL = "/static/"
+STATIC_ROOT = STATIC_ROOT = BASE_DIR / "static"
+# settings.py
+
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+
 ROOT_URLCONF = 'qnd41app.urls'
 LOCALE_PATHS =  (
     os.path.join(BASE_DIR, 'locale/'),
