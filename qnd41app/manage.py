831f31552760ef71c4ca51de6dbf952a27aefb32
smartquailstage <smartquail.stage@gmail.com>
2025-01-22 15:39:38 -0500

    jj

--- qnd41app/manage.py
+++ qnd41app/manage.py
@@ -1,13 +1,9 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
-import sys
-from dotenv import load_dotenv
-
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.stage')
+    # Se obtiene directamente de las variables de entorno
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.stage'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,9 +13,3 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-
-
-if __name__ == '__main__':
-
-    load_dotenv()
-    main()
