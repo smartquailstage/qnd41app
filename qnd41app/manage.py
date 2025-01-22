873278ef09f6f59802234a75705d0c53e7e43f97
smartquailstage <smartquail.stage@gmail.com>
2025-01-22 11:53:42 -0500

    Update manage.py

--- qnd41app/manage.py
+++ qnd41app/manage.py
@@ -1,25 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import sys
-from dotenv import load_dotenv
-
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.stage')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.pro')
+    from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
-
 if __name__ == '__main__':
-
-    load_dotenv()
     main()
