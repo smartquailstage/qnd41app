62f25259ab3bf6a7745c87b8331a9172e9a77a14
smartquailstage <smartquail.stage@gmail.com>
2025-01-22 00:03:24 -0500

    ef

--- qnd41app/manage.py
+++ qnd41app/manage.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.pro')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
