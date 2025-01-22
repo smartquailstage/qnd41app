d016834a1540367f47ea19f879cb3bd426428665
smartquailstage <smartquail.stage@gmail.com>
2025-01-22 13:19:49 -0500

    Update manage.py

--- qnd41app/manage.py
+++ qnd41app/manage.py
@@ -2,14 +2,7 @@ import os
 import sys
 
 def main():
-    # Definir explícitamente el módulo de configuración a utilizar
-    settings_module = 'qnd41app.settings.pro'  # Aquí defines de forma explícita el módulo de configuración pro.
-
-    # Establecer la variable de entorno DJANGO_SETTINGS_MODULE
-    # Esto le dice a Django qué archivo de configuración utilizar para este entorno.
-    os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
-
-    # Ahora que ya hemos configurado el módulo de settings, ejecutamos el comando de Django
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.pro')
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
 
