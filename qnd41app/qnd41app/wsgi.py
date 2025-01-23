27e5ef3c865ec389d28ae90915906eb35c937faf
smartquailstage <smartquail.stage@gmail.com>
2025-01-23 11:38:58 -0500

    Update wsgi.py

--- qnd41app/qnd41app/wsgi.py
+++ qnd41app/qnd41app/wsgi.py
@@ -1,10 +1,10 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-# Obtener la variable de entorno
+# Obtener la variable de entorno DJANGO_SETTINGS_MODULE, si no está, se usará el valor predeterminado.
 settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.local')
 
-# Configurar el entorno de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+# Configurar la variable de entorno DJANGO_SETTINGS_MODULE, si no está ya configurada.
+os.environ['DJANGO_SETTINGS_MODULE'] = settings_module
 
 application = get_wsgi_application()
