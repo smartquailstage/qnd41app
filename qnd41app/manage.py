715e92e1c7e62fd9c7059889efd5952939abec58
smartquailstage <smartquail.stage@gmail.com>
2025-01-23 11:26:05 -0500

    sfa

--- qnd41app/manage.py
+++ qnd41app/manage.py
@@ -4,7 +4,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('qnd41app.settings.local', 'qnd41app.settings.local')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.local')
     
     try:
         from django.core.management import execute_from_command_line
--- qnd41app/qnd41app/wsgi.py
+++ qnd41app/qnd41app/wsgi.py
@@ -1,25 +1,10 @@
-"""
-WSGI config for qnode30_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
-import pathlib
-from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-# Establecer la ruta al archivo .env en la carpeta de settings
-CURRENT_DIR = pathlib.Path(__file__).resolve().parent
-BASE_DIR = CURRENT_DIR.parent
-env_path = CURRENT_DIR / '.env_pro'
-load_dotenv(dotenv_path=env_path)
+# Obtener la variable de entorno
+settings_module = os.environ.get('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.local')
 
-# Establecer el módulo de configuración de Django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qnd41app.settings.pro')
+# Configurar el entorno de Django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
-# Crear la aplicación WSGI
 application = get_wsgi_application()
